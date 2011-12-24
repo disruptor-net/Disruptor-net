3e79ce9094c7ee9dec640f49932ea8681887d492
Olivier Deheurles <mail@odeheurles.com>
2011-12-24 18:46:08 +0000

    Added Volatile.ReferenceArray<T>

--- Atomic/Volatile.cs
+++ Atomic/Volatile.cs
@@ -1139,6 +1139,157 @@ namespace System.Threading
                 return value ? True : False;
             }
         }
+
+        /// <summary>
+        /// A reference array that may be updated atomically
+        /// </summary>
+        public class ReferenceArray<T> where T : class
+        {
+            private readonly T[] _array;
+
+            /// <summary>
+            /// Create a new <see cref="ReferenceArray{T}"/> of a given length
+            /// </summary>
+            /// <param name="length">Length of the array</param>
+            public ReferenceArray(int length)
+            {
+                if (length <= 0) throw new ArgumentOutOfRangeException("length");
+
+                _array = new T[length];
+            }
+
+            /// <summary>
+            ///  Create a new <see cref="ReferenceArray{T}"/>with the same length as, and all elements copied from, the given array.
+            /// </summary>
+            /// <param name="array"></param>
+            public ReferenceArray(T[] array)
+            {
+                if (array == null) throw new ArgumentNullException("array");
+
+                _array = array.ToArray();
+            }
+
+            /// <summary>
+            /// Length of the array
+            /// </summary>
+            public int Length
+            {
+                get { return _array.Length; }
+            }
+
+            /// <summary>
+            /// Read the value without applying any fence
+            /// </summary>
+            /// <param name="index">The index of the element.</param>
+            /// <returns>The current value.</returns>
+            public T ReadUnfenced(int index)
+            {
+                return _array[index];
+            }
+
+            /// <summary>
+            /// Read the value applying acquire fence semantic
+            /// </summary>
+            /// <param name="index">The element index</param>
+            /// <returns>The current value</returns>
+            public T ReadAcquireFence(int index)
+            {
+                var value = _array[index];
+                Thread.MemoryBarrier();
+                return value;
+            }
+
+            /// <summary>
+            /// Read the value applying full fence semantic
+            /// </summary>
+            /// <param name="index">The element index</param>
+            /// <returns>The current value</returns>
+            public T ReadFullFence(int index)
+            {
+                var value = _array[index];
+                Thread.MemoryBarrier();
+                return value;
+            }
+
+            /// <summary>
+            /// Read the value applying a compiler only fence, no CPU fence is applied
+            /// </summary>
+            /// <param name="index">The element index</param>
+            /// <returns>The current value</returns>
+            [MethodImpl(MethodImplOptions.NoOptimization)]
+            public T ReadCompilerOnlyFence(int index)
+            {
+                return _array[index];
+            }
+
+            /// <summary>
+            /// Write the value applying release fence semantic
+            /// </summary>
+            /// <param name="index">The element index</param>
+            /// <param name="newValue">The new value</param>
+            public void WriteReleaseFence(int index, T newValue)
+            {
+                _array[index] = newValue;
+                Thread.MemoryBarrier();
+            }
+
+            /// <summary>
+            /// Write the value applying full fence semantic
+            /// </summary>
+            /// <param name="index">The element index</param>
+            /// <param name="newValue">The new value</param>
+            public void WriteFullFence(int index, T newValue)
+            {
+                _array[index] = newValue;
+                Thread.MemoryBarrier();
+            }
+
+            /// <summary>
+            /// Write the value applying a compiler fence only, no CPU fence is applied
+            /// </summary>
+            /// <param name="index">The element index</param>
+            /// <param name="newValue">The new value</param>
+            [MethodImpl(MethodImplOptions.NoOptimization)]
+            public void WriteCompilerOnlyFence(int index, T newValue)
+            {
+                _array[index] = newValue;
+            }
+
+            /// <summary>
+            /// Write without applying any fence
+            /// </summary>
+            /// <param name="index">The index.</param>
+            /// <param name="newValue">The new value</param>
+            public void WriteUnfenced(int index, T newValue)
+            {
+                _array[index] = newValue;
+            }
+
+            /// <summary>
+            /// Atomically set the value to the given updated value if the current value equals the comparand
+            /// </summary>
+            /// <param name="newValue">The new value</param>
+            /// <param name="comparand">The comparand (expected value)</param>
+            /// <param name="index">The index.</param>
+            /// <returns>The original value</returns>
+            public bool AtomicCompareExchange(int index, T newValue, T comparand)
+            {
+                return Interlocked.CompareExchange(ref _array[index], newValue, comparand) == comparand;
+            }
+
+            /// <summary>
+            /// Atomically set the value to the given updated value
+            /// </summary>
+            /// <param name="newValue">The new value</param>
+            /// <param name="index">The index.</param>
+            /// <returns>The original value</returns>
+            public T AtomicExchange(int index, T newValue)
+            {
+                var result = Interlocked.Exchange(ref _array[index], newValue);
+                return result;
+            }
+        }
+
         #endregion
 
     }
