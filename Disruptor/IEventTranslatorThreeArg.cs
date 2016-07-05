3406b77bd78128cf898328d12770935cf6cca1b3
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-07-05 17:21:50 +0200

    Add comments to event translators

--- Disruptor/IEventTranslatorThreeArg.cs
+++ Disruptor/IEventTranslatorThreeArg.cs
@@ -1,16 +1,22 @@
 ﻿namespace Disruptor
 {
+    /// <summary>
+    /// Implementations translate another data representations into events claimed from the <see cref="RingBuffer{T}"/>
+    /// </summary>
+    /// <typeparam name="T">event implementation storing the data for sharing during exchange or parallel coordination of an event.</typeparam>
+    /// <typeparam name="A"></typeparam>
+    /// <typeparam name="B"></typeparam>
+    /// <typeparam name="C"></typeparam>
     public interface IEventTranslatorThreeArg<T, A, B, C>
     {
-        /**
-         * Translate a data representation into fields set in given event
-         *
-         * @param event    into which the data should be translated.
-         * @param sequence that is assigned to event.
-         * @param arg0     The first user specified argument to the translator
-         * @param arg1     The second user specified argument to the translator
-         * @param arg2     The third user specified argument to the translator
-         */
+        /// <summary>
+        /// Translate a data representation into fields set in given event
+        /// </summary>
+        /// <param name="event">into which the data should be translated.</param>
+        /// <param name="sequence">that is assigned to event.</param>
+        /// <param name="arg0"></param>
+        /// <param name="arg1"></param>
+        /// <param name="arg2"></param>
         void TranslateTo(T @event, long sequence, A arg0, B arg1, C arg2);
     }
 }
--- Disruptor/IEventTranslatorVararg.cs
+++ Disruptor/IEventTranslatorVararg.cs
@@ -1,20 +1,17 @@
 namespace Disruptor
 {
-    /**
- * Implementations translate another data representations into events claimed from the {@link RingBuffer}
- *
- * @param <T> event implementation storing the data for sharing during exchange or parallel coordination of an event.
- * @see EventTranslator
- */
+    /// <summary>
+    /// Implementations translate another data representations into events claimed from the <see cref="RingBuffer{T}"/>
+    /// </summary>
+    /// <typeparam name="T">event implementation storing the data for sharing during exchange or parallel coordination of an event.</typeparam>
     public interface IEventTranslatorVararg<T>
     {
-        /**
-         * Translate a data representation into fields set in given event
-         *
-         * @param event    into which the data should be translated.
-         * @param sequence that is assigned to event.
-         * @param args     The array of user arguments.
-         */
+        /// <summary>
+        /// Translate a data representation into fields set in given event
+        /// </summary>
+        /// <param name="event">into which the data should be translated.</param>
+        /// <param name="sequence">that is assigned to event.</param>
+        /// <param name="args">The array of user arguments.</param>
         void TranslateTo(T @event, long sequence, params object[] args);
     }
 }
