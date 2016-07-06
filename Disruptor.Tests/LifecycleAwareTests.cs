a250e2cb521b25f3e70397edabec60b7d84201cf
Antoine Blanchet <a.blanchet@abc-arbitrage.com>
2016-07-06 18:38:34 +0200

    ShutdownOnFatalExceptionTest updated

--- Disruptor.Tests/LifecycleAwareTests.cs
+++ Disruptor.Tests/LifecycleAwareTests.cs
@@ -7,9 +7,11 @@ namespace Disruptor.Tests
     [TestFixture]
     public class LifecycleAwareTests
     {
-        private readonly ManualResetEvent _startMru = new ManualResetEvent(false);
-        private readonly ManualResetEvent _shutdownMru = new ManualResetEvent(false);
-        private readonly RingBuffer<StubEvent> _ringBuffer = new RingBuffer<StubEvent>(()=>new StubEvent(-1), 16);
+        private readonly ManualResetEvent _startSignal = new ManualResetEvent(false);
+        private readonly ManualResetEvent _shutdownSignal = new ManualResetEvent(false);
+
+        private readonly RingBuffer<StubEvent> _ringBuffer = new RingBuffer<StubEvent>(() => new StubEvent(-1), 16);
+
         private ISequenceBarrier _sequenceBarrier;
         private LifecycleAwareEventHandler _eventHandler;
         private BatchEventProcessor<StubEvent> _batchEventProcessor;
@@ -18,20 +20,19 @@ namespace Disruptor.Tests
         public void SetUp()
         {
             _sequenceBarrier = _ringBuffer.NewBarrier();
-            _eventHandler = new LifecycleAwareEventHandler(_startMru, _shutdownMru);
+            _eventHandler = new LifecycleAwareEventHandler(_startSignal, _shutdownSignal);
             _batchEventProcessor = new BatchEventProcessor<StubEvent>(_ringBuffer, _sequenceBarrier, _eventHandler);
         }
 
         [Test]
-        public void ShouldNotifyOfEventProcessorLifecycle()
+        public void ShouldNotifyOfBatchProcessorLifecycle()
         {
             new Thread(_batchEventProcessor.Run).Start();
 
-            _startMru.WaitOne();
-
+            _startSignal.WaitOne();
             _batchEventProcessor.Halt();
 
-            _shutdownMru.WaitOne();
+            _shutdownSignal.WaitOne();
 
             Assert.AreEqual(_eventHandler.StartCounter, 1);
             Assert.AreEqual(_eventHandler.ShutdownCounter, 1);
@@ -39,25 +40,17 @@ namespace Disruptor.Tests
         
         private sealed class LifecycleAwareEventHandler : IEventHandler<StubEvent>, ILifecycleAware
         {
-            private readonly ManualResetEvent _startMru;
-            private readonly ManualResetEvent _shutdownMru;
-            private int _startCounter;
-            private int _shutdownCounter;
+            private readonly ManualResetEvent _startSignal;
+            private readonly ManualResetEvent _shutdownSignal;
 
-            public int StartCounter
-            {
-                get { return _startCounter; }
-            }
+            public int StartCounter { get; private set; }
 
-            public int ShutdownCounter
-            {
-                get { return _shutdownCounter; }
-            }
+            public int ShutdownCounter { get; private set; }
 
-            public LifecycleAwareEventHandler(ManualResetEvent startMru, ManualResetEvent shutdownMru)
+            public LifecycleAwareEventHandler(ManualResetEvent startSignal, ManualResetEvent shutdownSignal)
             {
-                _startMru = startMru;
-                _shutdownMru = shutdownMru;
+                _startSignal = startSignal;
+                _shutdownSignal = shutdownSignal;
             }
 
             public void OnEvent(StubEvent data, long sequence, bool endOfBatch)
@@ -66,14 +59,14 @@ namespace Disruptor.Tests
 
             public void OnStart()
             {
-                ++_startCounter;
-                _startMru.Set();
+                ++StartCounter;
+                _startSignal.Set();
             }
 
             public void OnShutdown()
             {
-                ++_shutdownCounter;
-                _shutdownMru.Set();
+                ++ShutdownCounter;
+                _shutdownSignal.Set();
             }
         }
     }    
---
+++ Disruptor.Tests/ShutdownOnFatalExceptionTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Disruptor.Dsl;
+
+namespace Disruptor.Tests
+{
+    [TestFixture]
+    public class ShutdownOnFatalExceptionTest
+    {
+        private readonly Random _random = new Random();
+        private readonly FailingEventHandler _failingEventHandler = new FailingEventHandler();
+        private Disruptor<byte[]> _disruptor;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _disruptor = new Disruptor<byte[]>(() => new byte[256], 1024, TaskScheduler.Current, ProducerType.Single, new BlockingWaitStrategy());
+            _disruptor.HandleEventsWith(_failingEventHandler);
+            _disruptor.SetDefaultExceptionHandler(new FatalExceptionHandler());
+        }
+
+        [Test, Timeout(1000)]
+        public void ShouldShutdownGracefulEvenWithFatalExceptionHandler()
+        {
+            _disruptor.Start();
+
+            for (var i = 1; i < 10; i++)
+            {
+                var bytes = new byte[32];
+                _random.NextBytes(bytes);
+                _disruptor.PublishEvent(new ByteArrayTranslator(bytes));
+            }
+        }
+
+        public class ByteArrayTranslator : IEventTranslator<byte[]>
+        {
+            private readonly byte[] _bytes;
+
+            public ByteArrayTranslator(byte[] bytes)
+            {
+                _bytes = bytes;
+            }
+
+            public void TranslateTo(byte[] eventData, long sequence)
+            {
+                _bytes.CopyTo(eventData, 0);
+            }
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _disruptor.Shutdown();
+        }
+
+
+        private class FailingEventHandler : IEventHandler<byte[]>
+        {
+            private int _count = 0;
+
+            public void OnEvent(byte[] data, long sequence, bool endOfBatch)
+            {
+                _count++;
+                if (_count == 3)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
