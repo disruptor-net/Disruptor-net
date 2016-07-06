2b2117dc880f646793ba70da9d4286bece965384
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-07-06 10:34:00 +0200

    Implement phased backoff wait strategy

---
+++ Disruptor/PhasedBackoffWaitStrategy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Disruptor
+{
+    /// <summary>
+    /// <para>Phased wait strategy for waiting <see cref="IEventProcessor"/>s on a barrier.</para>
+    /// 
+    /// <para>This strategy can be used when throughput and low-latency are not as important as CPU resource.
+    /// Spins, then yields, then waits using the configured fallback WaitStrategy.</para>
+    /// </summary>
+    public class PhasedBackoffWaitStrategy : IWaitStrategy
+    {
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.Winapi)]
+        private static extern void GetSystemTimePreciseAsFileTime(out long filetime);
+
+        private const int _spinTries = 10000;
+        private readonly IWaitStrategy _fallbackStrategy;
+        private readonly long _spinDurationInTicks;
+        private readonly long _yieldDurationInTicks;
+
+        public PhasedBackoffWaitStrategy(TimeSpan spinDuration, TimeSpan yieldDuration, IWaitStrategy fallbackStrategy)
+        {
+            _fallbackStrategy = fallbackStrategy;
+            _spinDurationInTicks = spinDuration.Ticks;
+            _yieldDurationInTicks = yieldDuration.Ticks;
+
+            try
+            {
+                long fileTime;
+                GetSystemTimePreciseAsFileTime(out fileTime);
+            }
+            catch (EntryPointNotFoundException)
+            {
+                // Not running Windows 8 or higher.
+                throw new ApplicationException("Precise system time is not available so sub-millisecond resolution is not available and this wait strategy is not recommended");
+            }
+        }
+
+        public long WaitFor(long sequence, Sequence cursor, Sequence dependentSequence, ISequenceBarrier barrier)
+        {
+            long startTime = 0;
+            int counter = _spinTries;
+
+            do
+            {
+                long availableSequence;
+                if ((availableSequence = dependentSequence.Value) >= sequence)
+                    return availableSequence;
+
+                if (0 == --counter)
+                {
+                    if (0 == startTime)
+                    {
+                        GetSystemTimePreciseAsFileTime(out startTime);
+                    }
+                    else
+                    {
+                        long fileTime;
+                        GetSystemTimePreciseAsFileTime(out fileTime);
+                        var timeDelta = fileTime - startTime;
+                        if (timeDelta > _yieldDurationInTicks)
+                            return _fallbackStrategy.WaitFor(sequence, cursor, dependentSequence, barrier);
+
+                        if (timeDelta > _spinDurationInTicks)
+                            Thread.Yield(); 
+                    }
+                    counter = _spinTries;
+                }
+            }
+            while (true);
+        }
+
+        public void SignalAllWhenBlocking()
+        {
+            _fallbackStrategy.SignalAllWhenBlocking();
+        }
+    }
+}
