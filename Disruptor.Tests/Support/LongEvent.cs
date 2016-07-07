869889a260df8fa4cae1fe76d12c45565d24da47
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-07-07 09:52:40 +0200

    Add BatchingTest

--- Disruptor.Tests/Support/LongEvent.cs
+++ Disruptor.Tests/Support/LongEvent.cs
@@ -1,12 +1,11 @@
+using System;
+
 namespace Disruptor.Tests.Support
 {
     public class LongEvent
     {
-        public LongEvent(long value)
-        {
-            Value = value;
-        }
-
         public long Value { get; set; }
+
+        public static Func<LongEvent> Factory => () => new LongEvent();
     }
 }
