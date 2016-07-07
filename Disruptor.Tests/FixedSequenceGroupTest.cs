6d6bdc29bceceec50d1609ef39b9541e2a4a7b44
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-07-07 15:49:49 +0200

    Add MultiProducerSequencerTest

---
+++ Disruptor.Tests/FixedSequenceGroupTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Disruptor.Tests
+{
+    [TestFixture]
+    public class FixedSequenceGroupTest
+    {
+        [Test]
+        public void ShouldReturnMinimumOf2Sequences()
+        {
+            Sequence sequence1 = new Sequence(34);
+            Sequence sequnece2 = new Sequence(47);
+            Sequence group = new FixedSequenceGroup(new Sequence[] { sequence1, sequnece2 });
+
+            Assert.That(group.Value, Is.EqualTo(34L));
+            sequence1.Value = 35;
+            Assert.That(group.Value, Is.EqualTo(35L));
+            sequence1.Value = 48;
+            Assert.That(group.Value, Is.EqualTo(47L));
+        }
+    }
+}
---
+++ Disruptor.Tests/MultiProducerSequencerTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Disruptor.Tests
+{
+    [TestFixture]
+    public class MultiProducerSequencerTest
+    {
+        private readonly Sequencer _publisher = new MultiProducerSequencer(1024, new BlockingWaitStrategy());
+
+        [Test]
+        public void ShouldOnlyAllowMessagesToBeAvailableIfSpecificallyPublished() 
+        {
+            _publisher.Publish(3);
+            _publisher.Publish(5);
+
+            Assert.That(_publisher.IsAvailable(0), Is.EqualTo(false));
+            Assert.That(_publisher.IsAvailable(1), Is.EqualTo(false));
+            Assert.That(_publisher.IsAvailable(2), Is.EqualTo(false));
+            Assert.That(_publisher.IsAvailable(3), Is.EqualTo(true));
+            Assert.That(_publisher.IsAvailable(4), Is.EqualTo(false));
+            Assert.That(_publisher.IsAvailable(5), Is.EqualTo(true));
+            Assert.That(_publisher.IsAvailable(6), Is.EqualTo(false));
+        }
+    }
+}
