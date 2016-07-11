0c4af6979be984d58996eaf1fbf0c176f56c91ca
Olivier Coanet <o.coanet@abc-arbitrage.com>
2016-07-11 15:18:53 +0200

    Increase PaddedLong size

--- Disruptor.Tests/Support/PaddedLong.cs
+++ Disruptor.Tests/Support/PaddedLong.cs
@@ -2,16 +2,10 @@
 
 namespace Disruptor.Tests.Support
 {
-    // TODO: expand to cache line and create a PR on java version
-    [StructLayout(LayoutKind.Explicit, Size = 56)]
+    [StructLayout(LayoutKind.Explicit, Size = 64)]
     public struct PaddedLong
     {
         [FieldOffset(0)]
         public long Value;
-
-        public PaddedLong(long value)
-        {
-            Value = value;
-        }
     }
 }
