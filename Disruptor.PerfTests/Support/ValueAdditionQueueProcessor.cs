5c5a6baa90cb3bf30c9401d3542a6c62f6b379d8
MendelMonteiro <mendel.monteiro@gmail.com>
2016-07-12 22:11:19 +0200

    Migrate OneToOneQueueThroughputTest

---
+++ Disruptor.PerfTests/Support/ValueAdditionQueueProcessor.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Disruptor.PerfTests.Support
+{
+    class ValueAdditionQueueProcessor
+    {
+        private volatile bool _running;
+        private long _value;
+        private long _sequence;
+        private ManualResetEvent _latch;
+
+        private readonly BlockingCollection<long> _blockingQueue;
+        private readonly long _count;
+
+        public ValueAdditionQueueProcessor(BlockingCollection<long> blockingQueue, long count)
+        {
+            _blockingQueue = blockingQueue;
+            _count = count;
+        }
+
+        public long GetValue()
+        {
+            return _value;
+        }
+
+        public void Reset(ManualResetEvent latch)
+        {
+            _value = 0L;
+            _sequence = 0L;
+            _latch = latch;
+        }
+
+        public void Halt()
+        {
+            _running = false;
+        }
+
+        public void Run()
+        {
+            _running = true;
+            while (true)
+            {
+                try
+                {
+                    var value = _blockingQueue.Take();
+                    _value += value;
+
+                    if (_sequence++ == _count)
+                    {
+                        _latch.Set();
+                    }
+                }
+                catch
+                {
+                    if (!_running)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
