642930b81e181658080d8db075d714ecb924b04d
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-07-13 14:44:24 +0200

    Migrate OneToThreeReleasingWorkerPoolThroughputTest

--- Disruptor.PerfTests/Support/LockFreeBoundedQueue.cs
+++ Disruptor.PerfTests/Support/LockFreeBoundedQueue.cs
@@ -4,7 +4,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace Disruptor.PerfTests.WorkHandler
+namespace Disruptor.PerfTests.Support
 {
     class LockFreeBoundedQueue<T> : IProducerConsumerCollection<T>
     {
