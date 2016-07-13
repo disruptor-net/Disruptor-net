07975fd225da22c3444b2b912bf61e4fd6c77e23
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-07-13 11:45:26 +0200

    Migrate OneToThreeWorkerPoolThroughputTest

---
+++ Disruptor.PerfTests/Support/EventCountingQueueProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+using Disruptor.Tests.Support;
+
+namespace Disruptor.PerfTests.Support
+{
+    public class EventCountingQueueProcessor
+    {
+        private volatile bool _running;
+        private readonly BlockingCollection<long> _blockingQueue;
+        private readonly PaddedLong[] _counters;
+        private readonly int _index;
+
+        public EventCountingQueueProcessor(
+            BlockingCollection<long> blockingQueue, PaddedLong[] counters, int index)
+        {
+            _blockingQueue = blockingQueue;
+            _counters = counters;
+            _index = index;
+        }
+
+        public void Halt()
+        {
+            _running = false;
+        }
+
+        public void Run()
+        {
+            _running = true;
+            while (_running)
+            {
+                long item;
+                if (!_blockingQueue.TryTake(out item))
+                    continue;
+
+                _counters[_index].Value = _counters[_index].Value + 1L;
+            }
+        }
+    }
+}
---
+++ Disruptor.PerfTests/Support/EventCountingWorkHandler.cs
@@ -0,0 +1,21 @@
+﻿using Disruptor.Tests.Support;
+
+namespace Disruptor.PerfTests.Support
+{
+    public class EventCountingWorkHandler : IWorkHandler<ValueEvent>
+    {
+        private readonly PaddedLong[] _counters;
+        private readonly int _index;
+
+        public EventCountingWorkHandler(PaddedLong[] counters, int index)
+        {
+            _counters = counters;
+            _index = index;
+        }
+
+        public void OnEvent(ValueEvent evt)
+        {
+            _counters[_index].Value = _counters[_index].Value + 1L;
+        }
+    }
+}
