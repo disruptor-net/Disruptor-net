019e0b7521126fa8d437d5a076ec6fbb3ba7c8be
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-07-13 14:46:26 +0200

    Move ValueAdditionBatchQueueProcessor into support folder

--- Disruptor.PerfTests/Support/ValueAdditionBatchQueueProcessor.cs
+++ Disruptor.PerfTests/Support/ValueAdditionBatchQueueProcessor.cs
@@ -3,7 +3,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 
-namespace Disruptor.PerfTests.Sequenced
+namespace Disruptor.PerfTests.Support
 {
     class ValueAdditionBatchQueueProcessor
     {
