f6b60471d3f18c8306c46a37d6ecdca93f9db066
Olivier Coanet <o.coanet@abc-arbitrage.com>
2016-07-14 18:09:57 +0200

    Reorder RingBuffer fields to reduce size

--- Disruptor/RingBufferFields.cs
+++ Disruptor/RingBufferFields.cs
@@ -2,21 +2,21 @@ using System.Runtime.InteropServices;
 
 namespace Disruptor
 {
-    [StructLayout(LayoutKind.Explicit, Size = 144)]
+    [StructLayout(LayoutKind.Explicit, Size = 136)]
     internal struct RingBufferFields
     {
         // 56: protected long p1, p2, p3, p4, p5, p6, p7;
 
         [FieldOffset(56)]
-        public int IndexMask;
-
-        [FieldOffset(64)]
         public object[] Entries;
 
-        [FieldOffset(72)]
+        [FieldOffset(64)]
+        public int IndexMask;
+
+        [FieldOffset(68)]
         public int BufferSize;
 
-        [FieldOffset(80)]
+        [FieldOffset(72)]
         public ISequencer Sequencer;
 
         // 56: protected long p1, p2, p3, p4, p5, p6, p7;
