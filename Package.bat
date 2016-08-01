8df38911f648aa6b021841c5d18572da8a1be052
Mendel Monteiro-Beckerman <m.monteiro@abc-arbitrage.com>
2016-08-01 13:35:05 +0200

    Separate assembly version from nuget version and update the nuget package spec

--- Package.bat
+++ Package.bat
@@ -1,5 +1,5 @@
 rmdir /S /Q Target
 
-"C:\Program Files (x86)"\MSBuild\14.0\bin\msbuild Build.xml /t:Package /p:DisruptorVersion=3.3.4
+"C:\Program Files (x86)"\MSBuild\14.0\bin\msbuild Build.xml /t:Package /p:DisruptorVersion=3.3.4 /p:NugetVersion=3.3.4-pre
 
 pause
