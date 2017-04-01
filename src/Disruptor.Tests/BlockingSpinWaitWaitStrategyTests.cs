4c481857328d2b0ca3bbb5fd8d76db3ddcbdff86
Olivier Coanet <olivier.coanet@gmail.com>
2017-04-01 16:41:41 +0200

    Add a new blocking low CPU usage wait strategy

---
+++ src/Disruptor.Tests/BlockingSpinWaitWaitStrategyTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Disruptor.Tests
+{
+    [TestFixture]
+    public class BlockingSpinWaitWaitStrategyTests
+    {
+        [Test]
+        public void ShouldWaitForValue()
+        {
+            WaitStrategyTestUtil.AssertWaitForWithDelayOf(50, new BlockingSpinWaitWaitStrategy());
+        }
+    }
+}
---
+++ src/Disruptor/BlockingSpinWaitWaitStrategy.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+
+namespace Disruptor
+{
+    /// <summary>
+    /// Blocking strategy that uses a lock and condition variable for <see cref="IEventProcessor"/>s waiting on a barrier.
+    /// This strategy uses a <see cref="SpinWait"/> when waiting for the dependent sequence to prevent excessive CPU usage.
+    /// This strategy should be used when performance and low-latency are not as important as CPU resource.
+    /// </summary>
+    public sealed class BlockingSpinWaitWaitStrategy : IWaitStrategy
+    {
+        private readonly object _gate = new object();
+
+        /// <summary>
+        /// <see cref="IWaitStrategy.WaitFor"/>
+        /// </summary>
+        public long WaitFor(long sequence, Sequence cursor, ISequence dependentSequence, ISequenceBarrier barrier)
+        {
+            if (cursor.Value < sequence)
+            {
+                lock (_gate)
+                {
+                    while (cursor.Value < sequence)
+                    {
+                        barrier.CheckAlert();
+                        Monitor.Wait(_gate);
+                    }
+                }
+            }
+
+            var spinWait = new SpinWait();
+            long availableSequence;
+            while ((availableSequence = dependentSequence.Value) < sequence)
+            {
+                barrier.CheckAlert();
+                spinWait.SpinOnce();
+            }
+
+            return availableSequence;
+        }
+
+        /// <summary>
+        /// <see cref="IWaitStrategy.SignalAllWhenBlocking"/>
+        /// </summary>
+        public void SignalAllWhenBlocking()
+        {
+            lock (_gate)
+            {
+                Monitor.PulseAll(_gate);
+            }
+        }
+    }
+}
