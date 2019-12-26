e7642b8a1dd151b4c2e7aaeea1fe20bda0e19fe7
Olivier Coanet <o.coanet@abc-arbitrage.com>
2019-12-26 15:45:54 +0100

    Move SequencerDispatcher

--- src/Disruptor/SequencerDispatcher.cs
+++ src/Disruptor/SequencerDispatcher.cs
@@ -1,7 +1,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-namespace Disruptor.Internal
+namespace Disruptor
 {
     [StructLayout(LayoutKind.Explicit, Size = 16)]
     public readonly struct SequencerDispatcher
