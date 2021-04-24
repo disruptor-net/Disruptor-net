def76925eb5226924ca1c361a4dad6fcd0303bab
Olivier Coanet <o.coanet@abc-arbitrage.com>
2021-04-24 13:54:41 +0200

    Add benchmarks for Util.GetMinimumSequence

---
+++ src/Disruptor.Benchmarks/Util/GetMinimumSequenceBenchmarks.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+
+namespace Disruptor.Benchmarks
+{
+    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+    public class GetMinimumSequenceBenchmarks
+    {
+        private readonly ISequence[] _sequencesSize1;
+        private readonly ISequence[] _sequencesSize3;
+
+        public GetMinimumSequenceBenchmarks()
+        {
+            _sequencesSize1 = new ISequence[]
+            {
+                new Sequence(1),
+            };
+            _sequencesSize3 = new ISequence[]
+            {
+                new Sequence(100),
+                new Sequence(1),
+                new Sequence(1000),
+            };
+        }
+
+        [Benchmark, BenchmarkCategory("SizeOf1")]
+        public long GetMinimumSequence1()
+        {
+            return UtilGetMinimumSequence(_sequencesSize1);
+        }
+
+        [Benchmark(Baseline = true), BenchmarkCategory("SizeOf1")]
+        public long GetMinimumSequence1Ref()
+        {
+            return UtilGetMinimumSequenceRef(_sequencesSize1);
+        }
+
+        [Benchmark, BenchmarkCategory("SizeOf3")]
+        public long GetMinimumSequence3()
+        {
+            return UtilGetMinimumSequence(_sequencesSize3);
+        }
+
+        [Benchmark(Baseline = true), BenchmarkCategory("SizeOf3")]
+        public long GetMinimumSequence3Ref()
+        {
+            return UtilGetMinimumSequenceRef(_sequencesSize3);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long UtilGetMinimumSequence(ISequence[] sequences, long minimum = long.MaxValue)
+        {
+            for (var i = 0; i < sequences.Length; i++)
+            {
+                var sequence = sequences[i].Value;
+                if (sequence < minimum)
+                    minimum = sequence;
+            }
+
+            return minimum;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long UtilGetMinimumSequenceRef(ISequence[] sequences, long minimum = long.MaxValue)
+        {
+            for (var i = 0; i < sequences.Length; i++)
+            {
+                var sequence = sequences[i].Value;
+                minimum = Math.Min(minimum, sequence);
+            }
+
+            return minimum;
+        }
+    }
+}
