eab7ad20d44e3773dd88f202fdce2ca967575b1c
Olivier Coanet <o.coanet@abc-arbitrage.com>
2021-12-06 13:34:33 +0100

    Add event processor benchmarks

---
+++ src/Disruptor.Benchmarks/EventProcessorBenchmarks_WaitRaw.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+
+namespace Disruptor.Benchmarks
+{
+    /// <summary>
+    /// Compares event processing loops to identify the cost of <c>if (waitResult.IsTimeout)</c>.
+    /// </summary>
+    internal class EventProcessorBenchmarks_WaitRaw
+    {
+        private const int _operationsPerInvoke = 500;
+
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private readonly Sequence _sequence = new Sequence();
+        private readonly Sequence _cursor = new Sequence();
+        private readonly YieldingWaitStrategy _waitStrategy = new YieldingWaitStrategy();
+        private readonly ISequence _dependentSequences;
+
+        public EventProcessorBenchmarks_WaitRaw()
+        {
+            _dependentSequences = SequenceGroups.CreateReadOnlySequence(_cursor, Array.Empty<ISequence>());
+            _cursor.SetValue(42);
+        }
+
+        public int NextSequence { get; set; } = 42;
+
+        [Benchmark(Baseline = true, OperationsPerInvoke = _operationsPerInvoke)]
+        public void WaitWithCheck()
+        {
+            for (var i = 0; i < _operationsPerInvoke; i++)
+            {
+                var waitResult = _waitStrategy.WaitFor(NextSequence, _cursor, _dependentSequences, _cancellationTokenSource.Token);
+                if (waitResult.IsTimeout)
+                {
+                    HandleTimeout(_sequence.Value);
+                    return;
+                }
+
+                var availableSequence = waitResult.UnsafeAvailableSequence;
+                Process(availableSequence);
+
+                _sequence.SetValue(availableSequence);
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = _operationsPerInvoke)]
+        public void WaitWithoutCheck()
+        {
+            for (var i = 0; i < _operationsPerInvoke; i++)
+            {
+                var waitResult = _waitStrategy.WaitFor(NextSequence, _cursor, _dependentSequences, _cancellationTokenSource.Token);
+
+                var availableSequence = waitResult.UnsafeAvailableSequence;
+                Process(availableSequence);
+
+                _sequence.SetValue(availableSequence);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void HandleTimeout(long sequence)
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void Process(long sequence)
+        {
+        }
+    }
+}
