cbb372ae791b394599d4ea36cace9da5f91e27d9
Olivier Coanet <o.coanet@abc-arbitrage.com>
2021-12-06 14:05:43 +0100

    Adjust SequencerDispatcherBenchmarks
    Use YieldingWaitStrategy instead of the default BlockingWaitStrategy.
    
    Most of the time was spent in SignalAllWhenBlocking, making the benchmark
    almost useless.

--- src/Disruptor.Benchmarks/SequencerDispatcherBenchmarks.cs
+++ src/Disruptor.Benchmarks/SequencerDispatcherBenchmarks.cs
@@ -4,27 +4,47 @@ namespace Disruptor.Benchmarks
 {
     public class SequencerDispatcherBenchmarks
     {
+        private const int _operationsPerInvoke = 100;
+
+        private readonly SingleProducerSequencer _singleProducerSequencer;
         private readonly ISequencer _sequencer;
         private readonly SequencerDispatcher _dispatcher;
 
         public SequencerDispatcherBenchmarks()
         {
-            _sequencer = new SingleProducerSequencer(1024);
+            _singleProducerSequencer = new SingleProducerSequencer(1024, new YieldingWaitStrategy());
+            _sequencer = _singleProducerSequencer;
             _dispatcher = new SequencerDispatcher(_sequencer);
         }
 
-        [Benchmark(Baseline = true)]
+        [Benchmark(Baseline = true, OperationsPerInvoke = _operationsPerInvoke)]
         public void UseInterface()
         {
-            var sequence = _sequencer.Next();
-            _sequencer.Publish(sequence);
+            for (var i = 0; i < _operationsPerInvoke; i++)
+            {
+                var sequence = _sequencer.Next();
+                _sequencer.Publish(sequence);
+            }
         }
 
-        [Benchmark]
+        [Benchmark(OperationsPerInvoke = _operationsPerInvoke)]
         public void UseDispatcher()
         {
-            var sequence = _dispatcher.Next();
-            _dispatcher.Publish(sequence);
+            for (var i = 0; i < _operationsPerInvoke; i++)
+            {
+                var sequence = _dispatcher.Next();
+                _dispatcher.Publish(sequence);
+            }
+        }
+
+        [Benchmark(OperationsPerInvoke = _operationsPerInvoke)]
+        public void UseDirectCalls()
+        {
+            for (var i = 0; i < _operationsPerInvoke; i++)
+            {
+                var sequence = _singleProducerSequencer.Next();
+                _singleProducerSequencer.Publish(sequence);
+            }
         }
     }
 }
