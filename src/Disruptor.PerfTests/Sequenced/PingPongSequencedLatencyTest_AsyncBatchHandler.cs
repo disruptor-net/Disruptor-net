fba4457ed454c94af1b64f28db8f3b6ec80f8a27
Olivier Coanet <o.coanet@abc-arbitrage.com>
2022-02-12 16:08:26 +0100

    Cleanup PingPongSequencedLatencyTest_AsyncBatchHandler

--- src/Disruptor.PerfTests/Sequenced/PingPongSequencedLatencyTest_AsyncBatchHandler.cs
+++ src/Disruptor.PerfTests/Sequenced/PingPongSequencedLatencyTest_AsyncBatchHandler.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
-using Disruptor.PerfTests.Support;
 using Disruptor.Processing;
 using HdrHistogram;
 
@@ -16,33 +15,33 @@ public class PingPongSequencedLatencyTest_AsyncBatchHandler : ILatencyTest
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    private readonly RingBuffer<PerfEvent> _pingBuffer;
-    private readonly RingBuffer<PerfEvent> _pongBuffer;
+    private readonly RingBuffer<PingPongEvent> _pingBuffer;
+    private readonly RingBuffer<PingPongEvent> _pongBuffer;
 
     private readonly IAsyncSequenceBarrier _pongBarrier;
     private readonly Pinger _pinger;
-    private readonly IAsyncEventProcessor<PerfEvent> _pingProcessor;
+    private readonly IAsyncEventProcessor<PingPongEvent> _pingProcessor;
 
     private readonly IAsyncSequenceBarrier _pingBarrier;
     private readonly Ponger _ponger;
-    private readonly IAsyncEventProcessor<PerfEvent> _pongProcessor;
+    private readonly IAsyncEventProcessor<PingPongEvent> _pongProcessor;
 
     public PingPongSequencedLatencyTest_AsyncBatchHandler()
     {
-        _pingBuffer = RingBuffer<PerfEvent>.CreateSingleProducer(PerfEvent.EventFactory, _bufferSize, new AsyncWaitStrategy());
-        _pongBuffer = RingBuffer<PerfEvent>.CreateSingleProducer(PerfEvent.EventFactory, _bufferSize, new AsyncWaitStrategy());
+        _pingBuffer = RingBuffer<PingPongEvent>.CreateSingleProducer(() => new PingPongEvent(), _bufferSize, new AsyncWaitStrategy());
+        _pongBuffer = RingBuffer<PingPongEvent>.CreateSingleProducer(() => new PingPongEvent(), _bufferSize, new AsyncWaitStrategy());
 
         _pingBarrier = (IAsyncSequenceBarrier)_pingBuffer.NewBarrier();
         _pongBarrier = (IAsyncSequenceBarrier)_pongBuffer.NewBarrier();
 
-        _pinger = new Pinger(_pingBuffer, _iterations, _pauseNanos);
-        _ponger = new Ponger(_pongBuffer);
+        _pinger = new Pinger(_pongBuffer, _pauseNanos);
+        _ponger = new Ponger(_pingBuffer);
 
-        _pingProcessor = EventProcessorFactory.Create(_pongBuffer,_pongBarrier, _pinger);
-        _pongProcessor = EventProcessorFactory.Create(_pingBuffer,_pingBarrier, _ponger);
+        _pingProcessor = EventProcessorFactory.Create(_pingBuffer, _pingBarrier, _pinger);
+        _pongProcessor = EventProcessorFactory.Create(_pongBuffer, _pongBarrier, _ponger);
 
-        _pingBuffer.AddGatingSequences(_pongProcessor.Sequence);
-        _pongBuffer.AddGatingSequences(_pingProcessor.Sequence);
+        _pingBuffer.AddGatingSequences(_pingProcessor.Sequence);
+        _pongBuffer.AddGatingSequences(_pongProcessor.Sequence);
     }
 
     public int RequiredProcessorCount => 2;
@@ -51,21 +50,22 @@ public class PingPongSequencedLatencyTest_AsyncBatchHandler : ILatencyTest
 
     public void Run(Stopwatch stopwatch, HistogramBase histogram)
     {
-        var globalSignal = new CountdownEvent(3);
-        var signal = new ManualResetEvent(false);
-        _pinger.Reset(globalSignal, signal, histogram);
-        _ponger.Reset(globalSignal);
+        var startCountdown = new CountdownEvent(3);
+        var completedSignal = new ManualResetEvent(false);
+        _pinger.Reset(startCountdown, completedSignal, histogram);
+        _ponger.Reset(startCountdown);
 
         var processorTask1 = _pongProcessor.Start();
         var processorTask2 = _pingProcessor.Start();
         _pongProcessor.WaitUntilStarted(TimeSpan.FromSeconds(5));
         _pingProcessor.WaitUntilStarted(TimeSpan.FromSeconds(5));
 
-        globalSignal.Signal();
-        globalSignal.Wait();
+        startCountdown.Signal();
+        startCountdown.Wait();
         stopwatch.Start();
-        // running here
-        signal.WaitOne();
+
+        completedSignal.WaitOne();
+
         stopwatch.Stop();
 
         _pingProcessor.Halt();
@@ -73,57 +73,59 @@ public class PingPongSequencedLatencyTest_AsyncBatchHandler : ILatencyTest
         Task.WaitAll(processorTask1, processorTask2);
     }
 
-    private class Pinger : IAsyncBatchEventHandler<PerfEvent>
+    private class PingPongEvent
     {
-        private readonly RingBuffer<PerfEvent> _buffer;
-        private readonly long _maxEvents;
+        public long Counter;
+    }
+
+    private class Pinger : IAsyncBatchEventHandler<PingPongEvent>
+    {
+        private readonly RingBuffer<PingPongEvent> _buffer;
         private readonly long _pauseTimeNs;
         private readonly long _pauseTimeTicks;
         private HistogramBase _histogram;
+        private CountdownEvent _startCountdown;
+        private ManualResetEvent _completedSignal;
         private long _t0;
         private long _counter;
-        private CountdownEvent _globalSignal;
-        private ManualResetEvent _signal;
 
-        public Pinger(RingBuffer<PerfEvent> buffer, long maxEvents, long pauseTimeNs)
+        public Pinger(RingBuffer<PingPongEvent> buffer, long pauseTimeNs)
         {
             _buffer = buffer;
-            _maxEvents = maxEvents;
             _pauseTimeNs = pauseTimeNs;
             _pauseTimeTicks = LatencyTestSession.ConvertNanoToStopwatchTicks(pauseTimeNs);
         }
 
-        public ValueTask OnBatch(EventBatch<PerfEvent> batch, long sequence)
+        public ValueTask OnBatch(EventBatch<PingPongEvent> batch, long sequence)
         {
             foreach (var data in batch)
             {
                 var t1 = Stopwatch.GetTimestamp();
-
                 _histogram.RecordValueWithExpectedInterval(LatencyTestSession.ConvertStopwatchTicksToNano(t1 - _t0), _pauseTimeNs);
 
-                if (data.Value < _maxEvents)
+                if (data.Counter == _iterations)
                 {
-                    while (_pauseTimeTicks > (Stopwatch.GetTimestamp() - t1))
+                    _completedSignal.Set();
+                }
+                else
+                {
+                    while (_pauseTimeTicks > Stopwatch.GetTimestamp() - t1)
                     {
                         Thread.Yield();
                     }
 
-                    Send();
-                }
-                else
-                {
-                    _signal.Set();
+                    SendPing();
                 }
             }
 
             return ValueTask.CompletedTask;
         }
 
-        private void Send()
+        private void SendPing()
         {
             _t0 = Stopwatch.GetTimestamp();
             var next = _buffer.Next();
-            _buffer[next].Value = _counter;
+            _buffer[next].Counter = _counter;
             _buffer.Publish(next);
 
             _counter++;
@@ -131,44 +133,44 @@ public class PingPongSequencedLatencyTest_AsyncBatchHandler : ILatencyTest
 
         public void OnStart()
         {
-            _globalSignal.Signal();
-            _globalSignal.Wait();
+            _startCountdown.Signal();
+            _startCountdown.Wait();
 
             Thread.Sleep(1000);
 
-            Send();
+            SendPing();
         }
 
         public void OnShutdown()
         {
         }
 
-        public void Reset(CountdownEvent globalSignal, ManualResetEvent signal, HistogramBase histogram)
+        public void Reset(CountdownEvent startCountdown, ManualResetEvent completedSignal, HistogramBase histogram)
         {
+            _startCountdown = startCountdown;
+            _completedSignal = completedSignal;
             _histogram = histogram;
-            _globalSignal = globalSignal;
-            _signal = signal;
-
             _counter = 0;
         }
     }
 
-    private class Ponger : IAsyncBatchEventHandler<PerfEvent>
+    private class Ponger : IAsyncBatchEventHandler<PingPongEvent>
     {
-        private readonly RingBuffer<PerfEvent> _buffer;
-        private CountdownEvent _globalSignal;
+        private readonly RingBuffer<PingPongEvent> _buffer;
+        private CountdownEvent _startCountdown;
 
-        public Ponger(RingBuffer<PerfEvent> buffer)
+        public Ponger(RingBuffer<PingPongEvent> buffer)
         {
             _buffer = buffer;
         }
 
-        public ValueTask OnBatch(EventBatch<PerfEvent> batch, long sequence)
+        public ValueTask OnBatch(EventBatch<PingPongEvent> batch, long sequence)
         {
             foreach (var data in batch)
             {
                 var next = _buffer.Next();
-                _buffer[next].Value = data.Value;
+                var pingEvent = _buffer[next];
+                pingEvent.Counter = data.Counter;
                 _buffer.Publish(next);
             }
 
@@ -177,17 +179,17 @@ public class PingPongSequencedLatencyTest_AsyncBatchHandler : ILatencyTest
 
         public void OnStart()
         {
-            _globalSignal.Signal();
-            _globalSignal.Wait();
+            _startCountdown.Signal();
+            _startCountdown.Wait();
         }
 
         public void OnShutdown()
         {
         }
 
-        public void Reset(CountdownEvent globalSignal)
+        public void Reset(CountdownEvent startCountdown)
         {
-            _globalSignal = globalSignal;
+            _startCountdown = startCountdown;
         }
     }
 }
