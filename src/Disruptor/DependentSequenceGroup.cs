1008cba5343be6250cd2f05651b123cd28de286a
Olivier Coanet <o.coanet@abc-arbitrage.com>
2022-04-15 12:10:56 +0200

    Improve DependentSequenceGroup comments

--- src/Disruptor/DependentSequenceGroup.cs
+++ src/Disruptor/DependentSequenceGroup.cs
@@ -10,38 +10,43 @@ namespace Disruptor;
 /// </summary>
 /// <remarks>
 /// For a given set of event processors S1, the dependencies are the sequences of the processors that must run before S1.
-/// If S1 represents the first processors of the disruptor, then the only dependent sequence is the ring buffer cursor.
+/// If S1 represents the first processors of the disruptor, then the only dependency is the ring buffer cursor.
 /// </remarks>
 public class DependentSequenceGroup
 {
     private readonly Sequence _cursor;
-    private readonly Sequence[] _sequences;
-    private readonly ISequence[] _untypedSequences;
+    private readonly Sequence[] _dependencies;
+    private readonly ISequence[] _untypedDependencies;
 
-    public DependentSequenceGroup(Sequence cursor, params ISequence[] dependentSequences)
+    /// <summary>
+    /// Creates a new dependent sequence group.
+    /// </summary>
+    /// <param name="cursor">The ring buffer cursor</param>
+    /// <param name="dependencies">The sequences of the processors that must run before</param>
+    public DependentSequenceGroup(Sequence cursor, params ISequence[] dependencies)
     {
         _cursor = cursor;
 
         // The API exposes ISequence but the Disruptor code always uses the Sequence type for dependent sequences.
-        // So dependent sequence are expected to be instances of Sequence in the fast path.
+        // So, dependencies are expected to be instances of Sequence in the fast path.
 
         // To simply the implementation, either all sequences are Sequence and _sequences is used,
         // or _untypedSequences is used.
 
-        if (dependentSequences.Length == 0)
+        if (dependencies.Length == 0)
         {
-            _sequences = new[] { cursor };
-            _untypedSequences = Array.Empty<ISequence>();
+            _dependencies = new[] { cursor };
+            _untypedDependencies = Array.Empty<ISequence>();
         }
-        else if (dependentSequences.All(x => x is Sequence))
+        else if (dependencies.All(x => x is Sequence))
         {
-            _sequences = dependentSequences.Cast<Sequence>().ToArray();
-            _untypedSequences = Array.Empty<ISequence>();
+            _dependencies = dependencies.Cast<Sequence>().ToArray();
+            _untypedDependencies = Array.Empty<ISequence>();
         }
         else
         {
-            _sequences = Array.Empty<Sequence>();
-            _untypedSequences = dependentSequences.ToArray();
+            _dependencies = Array.Empty<Sequence>();
+            _untypedDependencies = dependencies.ToArray();
         }
     }
 
@@ -62,10 +67,10 @@ public class DependentSequenceGroup
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
-            if (_sequences.Length != 0)
+            if (_dependencies.Length != 0)
             {
                 var minimum = long.MaxValue;
-                foreach (var sequence in _sequences)
+                foreach (var sequence in _dependencies)
                 {
                     var sequenceValue = sequence.Value;
                     if (sequenceValue < minimum)
@@ -83,7 +88,7 @@ public class DependentSequenceGroup
     private long GetValueFromUntypedSequences()
     {
         var minimum = long.MaxValue;
-        foreach (var sequence in _untypedSequences)
+        foreach (var sequence in _untypedDependencies)
         {
             var sequenceValue = sequence.Value;
             if (sequenceValue < minimum)
