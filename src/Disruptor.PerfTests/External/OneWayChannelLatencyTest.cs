59ecc7fcec7c5aa91cc3ddbe5d9d7d751b7dc204
Olivier Coanet <o.coanet@abc-arbitrage.com>
2022-09-30 13:04:04 +0200

    Add one-way latency tests

---
+++ src/Disruptor.PerfTests/External/OneWayChannelLatencyTest.cs
@@ -0,0 +1,137 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Disruptor.PerfTests.Support;
+using Disruptor.Util;
+using HdrHistogram;
+
+namespace Disruptor.PerfTests.External;
+
+public class OneWayChannelLatencyTest : ILatencyTest
+{
+    private const int _bufferSize = 1024;
+    private const long _iterations = 100 * 1000 * 30;
+    private static readonly long _pause = StopwatchUtil.GetTimestampFromMicroseconds(10);
+
+    private readonly Producer _producer;
+    private readonly Consumer _consumer;
+
+    public OneWayChannelLatencyTest()
+    {
+        var channel = Channel.CreateBounded<PerfEvent>(new BoundedChannelOptions(_bufferSize)
+        {
+            FullMode = BoundedChannelFullMode.Wait,
+            SingleReader = true,
+            SingleWriter = true,
+        });
+        _producer = new Producer(channel);
+        _consumer = new Consumer(channel);
+    }
+
+    public int RequiredProcessorCount => 2;
+
+    public void Run(Stopwatch stopwatch, HistogramBase histogram)
+    {
+        var startSignal = new ManualResetEventSlim();
+        var producerTask = _producer.Start(startSignal);
+        var consumerTask = _consumer.Start(histogram);
+
+        _producer.Started.Wait();
+        _consumer.Started.Wait();
+
+        stopwatch.Start();
+
+        startSignal.Set();
+
+        producerTask.Wait();
+        consumerTask.Wait();
+
+        stopwatch.Stop();
+    }
+
+    private class Producer
+    {
+        private readonly Channel<PerfEvent> _channel;
+
+        public Producer(Channel<PerfEvent> channel)
+        {
+            _channel = channel;
+        }
+
+        public ManualResetEventSlim Started { get; } = new();
+
+        public Task Start(ManualResetEventSlim startSignal)
+        {
+            Started.Reset();
+
+            return Task.Run(() => Run(startSignal));
+        }
+
+        private async Task Run(ManualResetEventSlim startSignal)
+        {
+            Started.Set();
+
+            startSignal.Wait();
+
+            var pause = _pause;
+            var next = Stopwatch.GetTimestamp() + pause;
+
+            for (int i = 0; i < _iterations; i++)
+            {
+                var now = Stopwatch.GetTimestamp();
+                while (now < next)
+                {
+                    Thread.Yield();
+                    now = Stopwatch.GetTimestamp();
+                }
+
+                await _channel.Writer.WriteAsync(new PerfEvent { Value = now });
+
+                next = now + pause;
+            }
+
+            await _channel.Writer.WriteAsync(new PerfEvent { Value = -1 });
+        }
+    }
+
+    private class Consumer
+    {
+        private readonly Channel<PerfEvent> _channel;
+        private HistogramBase _histogram;
+
+        public Consumer(Channel<PerfEvent> channel)
+        {
+            _channel = channel;
+        }
+
+        public ManualResetEventSlim Started { get; } = new();
+
+        public Task Start(HistogramBase histogram)
+        {
+            Started.Reset();
+            _histogram = histogram;
+
+            return Task.Run(Run);
+        }
+
+        private async Task Run()
+        {
+            Started.Set();
+
+            while (await _channel.Reader.WaitToReadAsync().ConfigureAwait(false))
+            {
+                while (_channel.Reader.TryRead(out var perfEvent))
+                {
+                    if (perfEvent.Value == -1)
+                        return;
+
+                    var consumerTimestamp = Stopwatch.GetTimestamp();
+                    var duration = consumerTimestamp - perfEvent.Value;
+
+                    _histogram.RecordValue(StopwatchUtil.ToNanoseconds(duration));
+                }
+            }
+        }
+    }
+}
---
+++ src/Disruptor.PerfTests/Sequenced/OneWaySequencedLatencyTest_AsyncBatchHandler.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Disruptor.Dsl;
+using Disruptor.PerfTests.Support;
+using Disruptor.Util;
+using HdrHistogram;
+
+namespace Disruptor.PerfTests.Sequenced;
+
+public class OneWaySequencedLatencyTest_AsyncBatchHandler : ILatencyTest, IDisposable
+{
+    private const int _bufferSize = 1024;
+    private const long _iterations = 100 * 1000 * 30;
+    private static readonly long _pause = StopwatchUtil.GetTimestampFromMicroseconds(10);
+
+    private readonly Disruptor<PerfEvent> _disruptor;
+    private readonly Handler _handler;
+
+    public OneWaySequencedLatencyTest_AsyncBatchHandler()
+    {
+        _disruptor = new Disruptor<PerfEvent>(() => new PerfEvent(), _bufferSize, new AsyncWaitStrategy());
+        _handler = new Handler();
+        _disruptor.HandleEventsWith(_handler);
+        _disruptor.Start();
+    }
+
+    public int RequiredProcessorCount => 2;
+
+    public void Run(Stopwatch stopwatch, HistogramBase histogram)
+    {
+        _handler.Initialize(histogram);
+        _handler.Started.Wait();
+
+        var pause = _pause;
+        var next = Stopwatch.GetTimestamp() + pause;
+
+        stopwatch.Start();
+
+        var ringBuffer = _disruptor.RingBuffer;
+
+        for (int i = 0; i < _iterations; i++)
+        {
+            var now = Stopwatch.GetTimestamp();
+            while (now < next)
+            {
+                Thread.Yield();
+                now = Stopwatch.GetTimestamp();
+            }
+
+            var s = ringBuffer.Next();
+            ringBuffer[s].Value = now;
+            ringBuffer.Publish(s);
+
+            next = now + pause;
+        }
+
+        var lastS = ringBuffer.Next();
+        ringBuffer[lastS].Value = -1;
+        ringBuffer.Publish(lastS);
+
+        stopwatch.Stop();
+    }
+
+    public void Dispose()
+    {
+        _disruptor.Shutdown();
+    }
+
+    private class Handler : IAsyncBatchEventHandler<PerfEvent>
+    {
+        private HistogramBase _histogram;
+
+        public ManualResetEventSlim Started { get; } = new();
+        public ManualResetEventSlim Completed { get; } = new();
+
+        public void OnStart()
+        {
+            Started.Set();
+        }
+
+        public void Initialize(HistogramBase histogram)
+        {
+            _histogram = histogram;
+            Completed.Reset();
+        }
+
+        public ValueTask OnBatch(EventBatch<PerfEvent> batch, long sequence)
+        {
+            foreach (var data in batch.AsSpan())
+            {
+                if (data.Value == -1)
+                {
+                    Completed.Set();
+                    continue;
+                }
+
+                var handlerTimestamp = Stopwatch.GetTimestamp();
+                var duration = handlerTimestamp - data.Value;
+
+                _histogram.RecordValue(StopwatchUtil.ToNanoseconds(duration));
+            }
+
+            return ValueTask.CompletedTask;
+        }
+    }
+}
