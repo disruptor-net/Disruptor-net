35e01a3ce6193790f9d855fb58e1fb7c395a3f15
Olivier Coanet <o.coanet@abc-arbitrage.com>
2022-09-30 15:20:32 +0200

    PerfTests: Make perf events larger when LARGE_PERF_EVENTS is defined

--- src/Disruptor.PerfTests/Support/PerfEvent.cs
+++ src/Disruptor.PerfTests/Support/PerfEvent.cs
@@ -7,4 +7,10 @@ public sealed class PerfEvent
     public static readonly Func<PerfEvent> EventFactory = () => new PerfEvent();
 
     public long Value;
+
+#if LARGE_PERF_EVENTS
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public long Output { get; set; }
+#endif
 }
--- src/Disruptor.PerfTests/Support/PerfValueEvent.cs
+++ src/Disruptor.PerfTests/Support/PerfValueEvent.cs
@@ -8,4 +8,10 @@ public struct PerfValueEvent
     public static readonly unsafe int Size = sizeof(PerfValueEvent);
 
     public long Value { get; set; }
+
+#if LARGE_PERF_EVENTS
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public long Output { get; set; }
+#endif
 }
