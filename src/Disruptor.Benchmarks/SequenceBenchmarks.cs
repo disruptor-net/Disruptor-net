7a6bce9473dcd666d83294d776465397338541b9
Olivier Coanet <olivier.coanet@gmail.com>
2025-07-21 17:01:17 +0200

    Add Sequence benchmarks with interface

--- src/Disruptor.Benchmarks/SequenceBenchmarks.cs
+++ src/Disruptor.Benchmarks/SequenceBenchmarks.cs
@@ -9,23 +9,43 @@ namespace Disruptor.Benchmarks;
 public unsafe class SequenceBenchmarks
 {
     private readonly long[] _buffer = GC.AllocateArray<long>(64, pinned: true);
-    private readonly Sequence _sequence = new(0);
+    private readonly SequenceE0 _sequence = new(0);
+    private readonly ISequence _sequenceInterface = new SequenceE0(0);
     private readonly SequenceE1 _sequenceE1Value = new(0);
     private readonly SequenceE1 _sequenceE1Pointer;
     private readonly SequenceE2 _sequenceE2;
     private readonly SequenceE3 _sequenceE3;
+    private readonly SequenceE0[] _sequenceArray;
+    private readonly ISequence[] _sequenceInterfaceArray;
+    private readonly SequenceE1[] _sequenceE1ValueArray;
+    private readonly SequenceE1[] _sequenceE1PointerArray;
+    private readonly SequenceE2[] _sequenceE2Array;
+    private readonly SequenceE3[] _sequenceE3Array;
 
     public SequenceBenchmarks()
     {
         _sequenceE1Pointer = new SequenceE1((long*)Unsafe.AsPointer(ref _buffer[8]));
         _sequenceE2 = new SequenceE2((long*)Unsafe.AsPointer(ref _buffer[16]));
         _sequenceE3 = new SequenceE3((long*)Unsafe.AsPointer(ref _buffer[24]));
+
+        _sequenceArray = [_sequence];
+        _sequenceInterfaceArray = [_sequenceInterface];
+        _sequenceE1ValueArray = [_sequenceE1Value];
+        _sequenceE1PointerArray = [_sequenceE1Pointer];
+        _sequenceE2Array = [_sequenceE2];
+        _sequenceE3Array = [_sequenceE3];
     }
 
     [Benchmark, BenchmarkCategory("Add")]
     public long Add_Baseline()
     {
         return _sequence.AddAndGet(1);
+    }
+
+    [Benchmark, BenchmarkCategory("Add")]
+    public long Add_Interface()
+    {
+        return _sequenceInterface.AddAndGet(1);
     }
 
     [Benchmark, BenchmarkCategory("Add")]
@@ -59,6 +79,12 @@ public unsafe class SequenceBenchmarks
     }
 
     [Benchmark, BenchmarkCategory("Increment")]
+    public long Increment_Interface()
+    {
+        return _sequenceInterface.IncrementAndGet();
+    }
+
+    [Benchmark, BenchmarkCategory("Increment")]
     public long Increment_E1_Value()
     {
         return _sequenceE1Value.IncrementAndGet();
@@ -87,6 +113,13 @@ public unsafe class SequenceBenchmarks
     {
         var value = _sequence.Value;
         return _sequence.CompareAndSet(value, value + 1);
+    }
+
+    [Benchmark, BenchmarkCategory("CompareAndSet")]
+    public bool CompareAndSet_Interface()
+    {
+        var value = _sequenceInterface.Value;
+        return _sequenceInterface.CompareAndSet(value, value + 1);
     }
 
     [Benchmark, BenchmarkCategory("CompareAndSet")]
@@ -124,6 +157,18 @@ public unsafe class SequenceBenchmarks
         for (var i = 0; i < 10_000; i++)
         {
             sum += _sequence.Value;
+        }
+
+        return sum;
+    }
+
+    [Benchmark(OperationsPerInvoke = 10_000), BenchmarkCategory("Read")]
+    public long Read_Interface()
+    {
+        var sum = 0L;
+        for (var i = 0; i < 10_000; i++)
+        {
+            sum += _sequenceInterface.Value;
         }
 
         return sum;
@@ -175,5 +220,137 @@ public unsafe class SequenceBenchmarks
         }
 
         return sum;
+    }
+
+    [Benchmark(OperationsPerInvoke = 100), BenchmarkCategory("GetMinimum")]
+    public long GetMinimum_Baseline()
+    {
+        var sum = 0L;
+        for (var i = 0; i < 100; i++)
+        {
+            sum += GetMinimumSequence(_sequenceArray, _sequence.Value);
+        }
+
+        return sum;
+    }
+
+    [Benchmark(OperationsPerInvoke = 100), BenchmarkCategory("GetMinimum")]
+    public long GetMinimum_Interface()
+    {
+        var sum = 0L;
+        for (var i = 0; i < 100; i++)
+        {
+            sum += GetMinimumSequence(_sequenceInterfaceArray, _sequenceInterface.Value);
+        }
+
+        return sum;
+    }
+
+    [Benchmark(OperationsPerInvoke = 100), BenchmarkCategory("GetMinimum")]
+    public long GetMinimum_E1_Value()
+    {
+        var sum = 0L;
+        for (var i = 0; i < 100; i++)
+        {
+            sum += GetMinimumSequence(_sequenceE1ValueArray, _sequenceE1Value.Value);
+        }
+
+        return sum;
+    }
+
+    [Benchmark(OperationsPerInvoke = 100), BenchmarkCategory("GetMinimum")]
+    public long GetMinimum_E1_Pointer()
+    {
+        var sum = 0L;
+        for (var i = 0; i < 100; i++)
+        {
+            sum += GetMinimumSequence(_sequenceE1PointerArray, _sequenceE1Pointer.Value);
+        }
+
+        return sum;
+    }
+
+    [Benchmark(OperationsPerInvoke = 100), BenchmarkCategory("GetMinimum")]
+    public long GetMinimum_E2()
+    {
+        var sum = 0L;
+        for (var i = 0; i < 100; i++)
+        {
+            sum += GetMinimumSequence(_sequenceE2Array, _sequenceE2.Value);
+        }
+
+        return sum;
+    }
+
+    [Benchmark(OperationsPerInvoke = 100), BenchmarkCategory("GetMinimum")]
+    public long GetMinimum_E3()
+    {
+        var sum = 0L;
+        for (var i = 0; i < 100; i++)
+        {
+            sum += GetMinimumSequence(_sequenceE3Array, _sequenceE3.Value);
+        }
+
+        return sum;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static long GetMinimumSequence(SequenceE0[] sequences, long minimum = long.MaxValue)
+    {
+        for (var i = 0; i < sequences.Length; i++)
+        {
+            var sequence = sequences[i].Value;
+            if (sequence < minimum)
+                minimum = sequence;
+        }
+        return minimum;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static long GetMinimumSequence(ISequence[] sequences, long minimum = long.MaxValue)
+    {
+        for (var i = 0; i < sequences.Length; i++)
+        {
+            var sequence = sequences[i].Value;
+            if (sequence < minimum)
+                minimum = sequence;
+        }
+        return minimum;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static long GetMinimumSequence(SequenceE1[] sequences, long minimum = long.MaxValue)
+    {
+        for (var i = 0; i < sequences.Length; i++)
+        {
+            var sequence = sequences[i].Value;
+            if (sequence < minimum)
+                minimum = sequence;
+        }
+        return minimum;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static long GetMinimumSequence(SequenceE2[] sequences, long minimum = long.MaxValue)
+    {
+        for (var i = 0; i < sequences.Length; i++)
+        {
+            var sequence = sequences[i].Value;
+            if (sequence < minimum)
+                minimum = sequence;
+        }
+        return minimum;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static long GetMinimumSequence(SequenceE3[] sequences, long minimum = long.MaxValue)
+    {
+        for (var i = 0; i < sequences.Length; i++)
+        {
+            var sequence = sequences[i].Value;
+            if (sequence < minimum)
+                minimum = sequence;
+        }
+        return minimum;
     }
 }
---
+++ src/Disruptor.Benchmarks/SequenceE0.cs
@@ -0,0 +1,166 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Disruptor.Util;
+
+namespace Disruptor.Benchmarks;
+
+public interface ISequence
+{
+    /// <summary>
+    /// Gets the sequence value.
+    /// </summary>
+    /// <remarks>
+    /// Performs a volatile read of the sequence value.
+    /// </remarks>
+    long Value
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get;
+    }
+
+    /// <summary>
+    /// Sets the sequence value.
+    /// </summary>
+    /// <remarks>
+    /// Performs an ordered write of this sequence. The intent is a Store/Store barrier between this write and any previous store.
+    /// </remarks>
+    /// <param name="value">The new value for the sequence.</param>
+    void SetValue(long value);
+
+    /// <summary>
+    /// Sets the sequence value.
+    /// </summary>
+    /// <remarks>
+    /// Performs a volatile write of this sequence. The intent is a Store/Store barrier between this write and any previous write
+    /// and a Store/Load barrier between this write and any subsequent volatile read.
+    /// </remarks>
+    /// <param name="value">The new value for the sequence.</param>
+    void SetValueVolatile(long value);
+
+    /// <summary>
+    /// Performs a compare and set operation on the sequence.
+    /// </summary>
+    /// <param name="expectedSequence">the expected value for the sequence</param>
+    /// <param name="nextSequence">the new value for the sequence</param>
+    /// <returns>true if the operation succeeds, false otherwise.</returns>
+    bool CompareAndSet(long expectedSequence, long nextSequence);
+
+    ///<summary>
+    /// Atomically increments the sequence by one.
+    ///</summary>
+    ///<returns>incremented sequence</returns>
+    long IncrementAndGet();
+
+    ///<summary>
+    /// Atomically adds the supplied value.
+    ///</summary>
+    ///<returns>incremented sequence</returns>
+    long AddAndGet(long value);
+}
+
+[StructLayout(LayoutKind.Explicit, Size = Constants.DefaultPadding * 2 + 8)]
+public class SequenceE0 : ISequence
+{
+    /// <summary>
+    /// Set to -1 as sequence starting point
+    /// </summary>
+    public const long InitialCursorValue = -1;
+
+    // padding: DefaultPadding
+
+    [FieldOffset(Constants.DefaultPadding)]
+    private long _value;
+
+    // padding: DefaultPadding
+
+    /// <summary>
+    /// Construct a new sequence counter that can be tracked across threads.
+    /// </summary>
+    /// <param name="initialValue">initial value for the counter</param>
+    public SequenceE0(long initialValue = InitialCursorValue)
+    {
+        _value = initialValue;
+    }
+
+    /// <summary>
+    /// Gets the sequence value.
+    /// </summary>
+    /// <remarks>
+    /// Performs a volatile read of the sequence value.
+    /// </remarks>
+    public long Value
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => Volatile.Read(ref _value);
+    }
+
+    /// <summary>
+    /// Sets the sequence value.
+    /// </summary>
+    /// <remarks>
+    /// Performs an ordered write of this sequence. The intent is a Store/Store barrier between this write and any previous store.
+    /// </remarks>
+    /// <param name="value">The new value for the sequence.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void SetValue(long value)
+    {
+        Volatile.Write(ref _value, value);
+    }
+
+    /// <summary>
+    /// Sets the sequence value.
+    /// </summary>
+    /// <remarks>
+    /// Performs a volatile write of this sequence. The intent is a Store/Store barrier between this write and any previous write
+    /// and a Store/Load barrier between this write and any subsequent volatile read.
+    /// </remarks>
+    /// <param name="value">The new value for the sequence.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void SetValueVolatile(long value)
+    {
+        Volatile.Write(ref _value, value);
+        Thread.MemoryBarrier();
+    }
+
+    /// <summary>
+    /// Performs a compare and set operation on the sequence.
+    /// </summary>
+    /// <param name="expectedSequence">the expected value for the sequence</param>
+    /// <param name="nextSequence">the new value for the sequence</param>
+    /// <returns>true if the operation succeeds, false otherwise.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public bool CompareAndSet(long expectedSequence, long nextSequence)
+    {
+        return Interlocked.CompareExchange(ref _value, nextSequence, expectedSequence) == expectedSequence;
+    }
+
+    /// <summary>
+    /// Value of the <see cref="Disruptor.Sequence"/> as a String.
+    /// </summary>
+    /// <returns>String representation of the sequence.</returns>
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+
+    ///<summary>
+    /// Atomically increments the sequence by one.
+    ///</summary>
+    ///<returns>incremented sequence</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public long IncrementAndGet()
+    {
+        return Interlocked.Increment(ref _value);
+    }
+
+    ///<summary>
+    /// Atomically adds the supplied value.
+    ///</summary>
+    ///<returns>incremented sequence</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public long AddAndGet(long value)
+    {
+        return Interlocked.Add(ref _value, value);
+    }
+}
