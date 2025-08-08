0961dcfed9cb37a1103128d19f286b848d2f2d15
Olivier Coanet <olivier.coanet@gmail.com>
2025-08-08 16:01:00 +0200

    Add TimeoutYieldingWaitStrategy

---
+++ src/Disruptor.Tests/TimeoutYieldingWaitStrategyTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Disruptor.Tests;
+
+public class TimeoutYieldingWaitStrategyTests : TimeoutWaitStrategyFixture<TimeoutYieldingWaitStrategy>
+{
+    protected override TimeoutYieldingWaitStrategy CreateWaitStrategy(TimeSpan timeout)
+    {
+        return new TimeoutYieldingWaitStrategy(timeout);
+    }
+}
