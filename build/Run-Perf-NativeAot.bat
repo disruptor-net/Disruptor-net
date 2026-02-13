d9a11f39d789636c2fd2e9a0b772838a71d34ffe
Olivier Coanet <olivier.coanet@gmail.com>
2026-02-13 16:31:35 +0100

    Configure CPU affinity in perf scripts

--- build/Run-Perf-NativeAot.bat
+++ build/Run-Perf-NativeAot.bat
@@ -1,4 +1,4 @@
 dotnet publish ../src/Disruptor.PerfTests --configuration Release --output ./bin/PerfTests-NativeAot
 dotnet publish ../src/Disruptor.Tests.IpcPublisher --framework net8.0 --configuration Release --output ./bin/IpcPublisher-NativeAot
-.\bin\PerfTests-NativeAot\Disruptor.PerfTests.exe  --target all --report false --ipc-publisher-path .\bin\IpcPublisher-NativeAot\Disruptor.Tests.IpcPublisher.exe
+.\bin\PerfTests-NativeAot\Disruptor.PerfTests.exe  --target all --report false --cpus 2,3,4,5 --ipc-publisher-path .\bin\IpcPublisher-NativeAot\Disruptor.Tests.IpcPublisher.exe
 pause
--- build/Run-Perf.bat
+++ build/Run-Perf.bat
@@ -1,2 +1,2 @@
-dotnet run --project ../src/Disruptor.PerfTests/ --configuration Release --target all --report false
+dotnet run --project ../src/Disruptor.PerfTests/ --configuration Release --target all --report false --cpus 2,3,4,5
 pause
